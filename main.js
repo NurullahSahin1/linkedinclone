278c838d624f39891fc0c8a1876ad917a877bf83
NurullahSahin1 <nsahininovation@gmail.com>
2023-10-23 12:22:08 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+let profileMenu = document.getElementById("profileMenu");
+function toggleMenu()
+{
+    profileMenu.classList.toggle("open-menu");
+}
+
+//Yan aktiviteyi kontrol etmek ve "Daha fazla/Daha az" bağlantısını güncellemek için kullanılan fonksiyon.
+let sideActivity = document.getElementById("sidebarActivity");
+let moreLink = document.getElementById("showMoreLink");
+
+function toggleActivity(){
+    sideActivity.classList.toggle("open-activity");
+}
+//Yan aktivite açıksa bağlantı metnini "Daha Az" olarak değiştir, aksi takdirde "Daha Fazla" olarak değiştir. 
+
+if(sideActivity.classList.contains("open-activity")){
+    moreLink.innerHTML = "Daha az <b>-</b>";
+} else {
+    moreLink.innerHTML = "Daha fazla <b>+</b>";
+}
